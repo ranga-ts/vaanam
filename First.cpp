474124657ef42bebcda4425d0f627797ac6ad998
ranga-ts <ranganathan_ts@yahoo.com>
2020-04-12 22:13:39 +0530

    {CR:TRY} Commit 2

--- First.cpp
+++ First.cpp
@@ -10,5 +10,6 @@ int main (void)
 	cout << "\r\nHello First File!\r\n";
 	cout << "\r\nAdding a new line to show a diff\r\n";
 	cout << "\r\nCommit 1\r\n";
+	cout << "\r\nCommit 2\r\n";
 	return 0;
 }
--- Second.cpp
+++ Second.cpp
@@ -6,5 +6,6 @@ int main (void)
 {
 	cout << "\r\n Hi Second File!\r\n";
 	cout << "\r\nCommit 1\r\n";
+	cout << "\r\nCommit 2\r\n";
 	return 0;
 }
